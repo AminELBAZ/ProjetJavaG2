c40910c6703ec12bbbad4b492ebedb63870e860a
AminELBAZ <elbaz.amin@gmail.com>
2018-02-08 23:22:17 +0100

    Check Login+ check adresse Serveur

--- src/client/ServerPanel.java
+++ src/client/ServerPanel.java
@@ -29,7 +29,7 @@ import javafx.scene.Scene;
 public class ServerPanel extends Parent {
 
     private Text titre;
-    private Text erreurPort;
+    private Text erreur;
     private TextArea port;
     private Button valid;
     private Text pseudo;
@@ -38,37 +38,37 @@ public class ServerPanel extends Parent {
     private TextArea inputAddress;
 
     public ServerPanel(Stage stage) {
-                                        // PSEUDO //
+        // PSEUDO //
         this.pseudo = new Text("Votre pseudo : ");
         this.pseudo.setLayoutX(100);
         this.pseudo.setLayoutY(50);
-        
+
         this.inputPseudo = new TextArea();
         this.inputPseudo.setLayoutX(300);
         this.inputPseudo.setLayoutY(25);
         this.inputPseudo.setPrefSize(100, 50);
 
-                                        // PORT //
+        // PORT //
         this.titre = new Text("N° du port (1024-49151) :");
         this.titre.setLayoutX(100);
         this.titre.setLayoutY(150);
-        
+
         this.port = new TextArea();
         this.port.setLayoutX(300);
         this.port.setLayoutY(125);
         this.port.setPrefSize(100, 50);
 
-        this.erreurPort = new Text("Numéro de port invalide, vérifiez votre saisie.");
-        this.erreurPort.setLayoutX(150);
-        this.erreurPort.setLayoutY(240);
-        this.erreurPort.setFill(Color.RED);
-        this.erreurPort.setVisible(false);
-        
-                                        // SERVEUR //
+        this.erreur = new Text("Numéro de port invalide, vérifiez votre saisie.");
+        this.erreur.setLayoutX(100);
+        this.erreur.setLayoutY(110);
+        this.erreur.setFill(Color.RED);
+        this.erreur.setVisible(false);
+
+        // SERVEUR //
         this.address = new Text("Adresse du serveur :");
         this.address.setLayoutX(100);
         this.address.setLayoutY(250);
-        
+
         this.inputAddress = new TextArea();
         this.inputAddress.setLayoutX(300);
         this.inputAddress.setLayoutY(225);
@@ -84,6 +84,20 @@ public class ServerPanel extends Parent {
                 String adresseServer = inputAddress.getText();
                 // Si la valeur saisie est un entier entre 1024 et 49151 on lance le serveur correspondant
                 if (estUnEntierValide(port.getText())) {
+                    if (inputPseudo.getText().equals("")) {
+                        erreur.setText("Veuillez saisir un pseudo !");
+                        erreur.setLayoutX(100);
+                        erreur.setLayoutY(25);
+                        erreur.setVisible(true);
+                        return;
+                    }
+                    if (adresseServer.equals("")) {
+                        erreur.setText("Veuillez saisir l'adresse du serveur !");
+                        erreur.setLayoutX(100);
+                        erreur.setLayoutY(215);
+                        erreur.setVisible(true);
+                        return;
+                    }
                     stage.close();
                     //Création du client
                     Client c = null;
@@ -94,16 +108,16 @@ public class ServerPanel extends Parent {
                     }
                     //Création du stage
                     Stage clientStage = new Stage();
-                    ClientPanel clientPanel = new ClientPanel(clientStage,c);
+                    ClientPanel clientPanel = new ClientPanel(clientStage, c);
                     Group root = new Group();
                     root.getChildren().add(clientPanel);
                     Scene scene = new Scene(root, 600, 500);
                     clientStage.setTitle("Mon Chat");
                     clientStage.setScene(scene);
                     clientStage.show();
-                // Sinon on affiche un message d'erreur
+                    // Sinon on affiche un message d'erreur
                 } else {
-                    erreurPort.setVisible(true);
+                    erreur.setVisible(true);
                 }
             }
         }
@@ -112,7 +126,7 @@ public class ServerPanel extends Parent {
         this.getChildren().add(this.pseudo);
         this.getChildren().add(this.titre);
         this.getChildren().add(this.port);
-        this.getChildren().add(this.erreurPort);
+        this.getChildren().add(this.erreur);
         this.getChildren().add(this.address);
         this.getChildren().add(this.inputAddress);
         this.getChildren().add(this.valid);
